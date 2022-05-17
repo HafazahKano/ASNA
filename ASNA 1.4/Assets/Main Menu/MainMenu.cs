92f36dc912a7d9c381399cdb49db0135e5b4bc21
haaaano <96539057+haaaano@users.noreply.github.com>
2022-05-17 15:01:53 +0700

    baking

--- ASNA 1.4/Assets/Main Menu/MainMenu.cs
+++ ASNA 1.4/Assets/Main Menu/MainMenu.cs
@@ -10,6 +10,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void Tutorial()
+    {
+        SceneManager.LoadScene(2);
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit");
