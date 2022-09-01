735e56b3d899a4a8a7470736df621909bffd87c3
R-Dans <101683568+HafazahKano@users.noreply.github.com>
2022-09-01 13:58:17 +0700

    ASNA kmipn

---
+++ ASNA 1.4/Assets/ParticleFollowPath.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleFollowPath : MonoBehaviour
+{
+    public string pathName;
+    public float time;
+
+    void Start()
+    {
+        //iTween.moveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "easetype", iTween.EaseType.caseInOutSine, "time"), time);
+    }
+}
